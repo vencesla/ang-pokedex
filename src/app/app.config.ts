664649342e6a533b38ee7c4b0892927137254dae
vencesla <admirmabiki@gmail.com>
2025-11-25 13:35:28 +0100

    page not found

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter, Routes } from '@angular/router';
 import { PokemonList } from './pokemon/pokemon-list/pokemon-list';
 import { PokemonProfile } from './pokemon/pokemon-profile/pokemon-profile';
+import { PageNotFound } from './page-not-found/page-not-found';
 
 const routes: Routes = [
-  { path: 'pokemons/:id', component: PokemonProfile },
-  { path: 'pokemons', component: PokemonList },
+  { path: 'pokemons/:id', component: PokemonProfile , title: 'Pokémon'},
+  { path: 'pokemons', component: PokemonList, title: 'Pokédex'},
   { path: '', redirectTo: 'pokemons', pathMatch: 'full'},
+  { path: '**', component: PageNotFound},
 ]
 export const appConfig: ApplicationConfig = {
   providers: [
